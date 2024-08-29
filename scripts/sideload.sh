5e6de1ee688f907db18ae3aeb7c31d226917726e
Kadin Sayani <kadin.sayani@canonical.com>
2024-08-29 07:37:09 -0600

    update bashrc

--- scripts/sideload.sh
+++ scripts/sideload.sh
@@ -1,11 +1,11 @@
-cd /home/ubuntu/canonical/lxd; make; cd /home/ubuntu
+cd /home/kadinsayani/canonical/lxd; make; cd /home/kadinsayani
 sudo snap remove --purge lxd
 sudo snap remove --purge lxd-installer
 sudo snap install lxd --channel=latest/edge
 
-sudo mv /home/ubuntu/go/bin/lxd /var/snap/lxd/common/lxd.debug
-sudo mv /home/ubuntu/go/bin/lxc /var/snap/lxd/common/lxc.debug
-sudo mount --bind /home/ubuntu/go/bin/lxd-agent /snap/lxd/current/bin/lxd-agent
+sudo mv /home/kadinsayani/go/bin/lxd /var/snap/lxd/common/lxd.debug
+sudo mv /home/kadinsayani/go/bin/lxc /var/snap/lxd/common/lxc.debug
+sudo mount --bind /home/kadinsayani/go/bin/lxd-agent /snap/lxd/current/bin/lxd-agent
 sudo systemctl reload snap.lxd.daemon
 sudo snap set lxd daemon.debug=true
 sudo systemctl reload snap.lxd.daemon
